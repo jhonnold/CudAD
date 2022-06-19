9a51e054b4557981ee9a9d88f6e348bc73a77348
Jay Honnold <jayhonnold@gmail.com>
2022-06-19 14:56:01 -0700

    Experiment 47

--- src/main.cu
+++ src/main.cu
@@ -29,7 +29,7 @@ int main() {
     init();
 
     const string data_path = "E:/berserk/training-data/n5k/";
-    const string output    = "./resources/runs/testing/";
+    const string output    = "./resources/runs/exp47/";
 
     // Load files
     vector<string> files {};
