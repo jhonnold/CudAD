4c3f8a6ba0a1fc0726dbb00205a4682ba59cff9e
Jay Honnold <jayhonnold@gmail.com>
2023-01-17 20:59:41 -0700

    Filter dupes

--- src/main.cu
+++ src/main.cu
@@ -20,24 +20,61 @@
 #include "misc/config.h"
 #include "dataset/shuffle.h"
 #include "trainer.h"
+#include "position/zobrist.h"
+#include "dataset/reader.h"
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+// static constexpr uint64_t HASH_SIZE = 1024 * 1024 * 1024;
+
 int main() {
     init();
 
-    const string data_path = "E:/berserk/training-data/koi-and-berk/";
-    const string output    = "./resources/runs/exp131/";
+    // PRNG rng(1070372);
+    // Zobrist zobrist(rng);
+
+    // vector<Key> hash;
+    // hash.resize(HASH_SIZE);
+
+    // uint64_t duplicates = 0;
+
+    // vector<string> files {};
+    // for (int i = 1; i <= 100; i++) {
+    //     auto ds = read<BINARY>("E:/berserk/training-data/exp134/exp134." + to_string(i) + ".bin");
+    //     DataSet output {};
+
+    //     for (Position pos : ds.positions) {
+    //         Key key = zobrist.get_key(pos);
+            
+    //         size_t idx = (size_t)(key & (HASH_SIZE - 1));
+    //         Key old = hash[idx];
+
+    //         if (key == old) {
+    //             duplicates++;
+    //         } else {
+    //             hash[idx] = key;
+    //             output.positions.push_back(pos);
+    //         }
+    //     }
+        
+    //     cout << "Duplicates: " << duplicates << endl;
+
+    //     output.header.position_count = output.positions.size();
+    //     write("E:/berserk/training-data/exp135/exp135." + to_string(i) + ".bin", output);
+    // }
+
+    const string data_path = "E:/berserk/training-data/exp135/";
+    const string output    = "./resources/runs/exp135/";
 
     // Load files
     vector<string> files {};
     for (int i = 1; i <= 100; i++)
-        files.push_back(data_path + "koi-and-berk." + to_string(i) + ".bin");
+        files.push_back(data_path + "exp135." + to_string(i) + ".bin");
 
-    Trainer<Berserk, 1000> trainer {};
+    Trainer<Berserk, 600> trainer {};
     trainer.fit(files, vector<string> {data_path + "validation.bin"}, output);
 
     close();
