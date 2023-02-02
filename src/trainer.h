c02ec07b4989c558af8df311f52cfcb44fe600a7
Jay Honnold <jayhonnold@gmail.com>
2023-02-01 17:44:40 -0700

    asdf

--- src/trainer.h
+++ src/trainer.h
@@ -99,7 +99,7 @@ class Trainer {
                        std::to_string(validation_loss)});
 
             if (epoch % 5 == 0) {
-                quantitize_deep(output + "nn-epoch" + std::to_string(epoch) + ".nnue", *network, 64, 16);
+                quantitize_deep(output + "nn-epoch" + std::to_string(epoch) + ".nnue", *network);
             }
             if (epoch % 10 == 0) {
                 network->saveWeights(output + "weights-epoch" + std::to_string(epoch) + ".nnue");
